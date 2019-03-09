e1b7ac0c6db1b67011ac1b7cbaf13ff5415782d3
kittitadA <kittitad.taz@gmail.com>
2019-03-09 21:20:01 +0700

    Updates

---
+++ precache-manifest.f47e70e0006acee94c91b5618e36e40f.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ae4728936a7c774fd03",
+    "url": "/resume/static/css/main.fab8b0a6.chunk.css"
+  },
+  {
+    "revision": "6ae4728936a7c774fd03",
+    "url": "/resume/static/js/main.6ae47289.chunk.js"
+  },
+  {
+    "revision": "fd78f13f2eeb6b64bd90",
+    "url": "/resume/static/js/runtime~main.fd78f13f.js"
+  },
+  {
+    "revision": "1bd1b36cc3cf0f140c4a",
+    "url": "/resume/static/js/2.1bd1b36c.chunk.js"
+  },
+  {
+    "revision": "3c1db105de61e79f2f3929dce5edf330",
+    "url": "/resume/static/media/facebook-square-brands.3c1db105.svg"
+  },
+  {
+    "revision": "a1223de504180a12574a17a1f30c5a08",
+    "url": "/resume/static/media/twitter-square-brands.a1223de5.svg"
+  },
+  {
+    "revision": "e95a955356e7ccab526319efa9a62d96",
+    "url": "/resume/static/media/google-plus-square-brands.e95a9553.svg"
+  },
+  {
+    "revision": "0dccd175333b4d5ce049b66acdbe6dc4",
+    "url": "/resume/static/media/LogoMakr_6Qvxr0.0dccd175.png"
+  },
+  {
+    "revision": "661d243239b963bda73efb93d06a80f0",
+    "url": "/resume/static/media/Image_profile.661d2432.jpg"
+  },
+  {
+    "revision": "99325d4efd59006419033a0523a5b0ba",
+    "url": "/resume/static/media/html5-brands.99325d4e.svg"
+  },
+  {
+    "revision": "38445e8d7100f8d3d62c71c7013a6a7c",
+    "url": "/resume/static/media/css3-alt-brands.38445e8d.svg"
+  },
+  {
+    "revision": "8db9cd685111f6d953eb15bc5a3685fb",
+    "url": "/resume/static/media/js-square-brands.8db9cd68.svg"
+  },
+  {
+    "revision": "d8cf77403db1c802576acc37627ff00b",
+    "url": "/resume/static/media/react-brands.d8cf7740.svg"
+  },
+  {
+    "revision": "5041651bac7dc3943825aeb4e6ab7a2c",
+    "url": "/resume/static/media/sass-brands.5041651b.svg"
+  },
+  {
+    "revision": "5423f3cbbd362af5446fa0fea3a9caed",
+    "url": "/resume/static/media/git-square-brands.5423f3cb.svg"
+  },
+  {
+    "revision": "cdc525fef1db60b5ba5c2524d80a06ce",
+    "url": "/resume/static/media/node-js-brands.cdc525fe.svg"
+  },
+  {
+    "revision": "7f82f130610e5bfff11e5b9b36d75487",
+    "url": "/resume/static/media/php-brands.7f82f130.svg"
+  },
+  {
+    "revision": "3bd576cbb6d2028a4fe7ec1c59578b80",
+    "url": "/resume/index.html"
+  }
+];
